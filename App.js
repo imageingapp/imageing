6190afb797cbacf9adf2668282e29b75693a4ec3
thehairy <thehairy@users.noreply.github.com>
2023-01-22 17:40:12 +0000

    Prettified Code!

--- App.js
+++ App.js
@@ -13,52 +13,56 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
 	return (
-        <>
-    		<NavigationContainer>
-    			<Tab.Navigator
-    				screenOptions={({ route }) => ({
-    					tabBarIcon: ({ focused, color, size }) => {
-    						let iconName;
+		<>
+			<NavigationContainer>
+				<Tab.Navigator
+					screenOptions={({ route }) => ({
+						tabBarIcon: ({ focused, color, size }) => {
+							let iconName;
 
-    						if (route.name === 'Home') {
-    							iconName = focused ? 'camera' : 'camera-outline';
-    						} else if (route.name === 'Gallery') {
-    							iconName = focused ? 'albums' : 'albums-outline';
-    						} else if (route.name === 'Settings') {
-    							iconName = focused
-    								? 'settings'
-    								: 'settings-outline';
-    						}
+							if (route.name === 'Home') {
+								iconName = focused
+									? 'camera'
+									: 'camera-outline';
+							} else if (route.name === 'Gallery') {
+								iconName = focused
+									? 'albums'
+									: 'albums-outline';
+							} else if (route.name === 'Settings') {
+								iconName = focused
+									? 'settings'
+									: 'settings-outline';
+							}
 
-    						// You can return any component that you like here!
-    						return (
-    							<Ionicons
-    								name={iconName}
-    								size={size}
-    								color={color}
-    							/>
-    						);
-    					},
-    					tabBarActiveTintColor: 'turquoise',
-    					tabBarInactiveTintColor: 'gray',
-    					headerShown: false,
-    					unmountOnBlur: true
-    				})}>
-    				<Tab.Screen
-    					name='Home'
-    					component={HomeScreen}
-    				/>
-    				<Tab.Screen
-    					name='Gallery'
-    					component={GalleryScreen}
-    				/>
-    				<Tab.Screen
-    					name='Settings'
-    					component={SettingScreen}
-    				/>
-    			</Tab.Navigator>
-    		</NavigationContainer>
-            <StatusBar style="dark" />
-        </>
-    );
+							// You can return any component that you like here!
+							return (
+								<Ionicons
+									name={iconName}
+									size={size}
+									color={color}
+								/>
+							);
+						},
+						tabBarActiveTintColor: 'turquoise',
+						tabBarInactiveTintColor: 'gray',
+						headerShown: false,
+						unmountOnBlur: true
+					})}>
+					<Tab.Screen
+						name='Home'
+						component={HomeScreen}
+					/>
+					<Tab.Screen
+						name='Gallery'
+						component={GalleryScreen}
+					/>
+					<Tab.Screen
+						name='Settings'
+						component={SettingScreen}
+					/>
+				</Tab.Navigator>
+			</NavigationContainer>
+			<StatusBar style='dark' />
+		</>
+	);
 }
--- Screens/Gallery.js
+++ Screens/Gallery.js
@@ -1,19 +1,19 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState, useEffect } from 'react';
 import {
-    View,
-    FlatList,
-    Image,
-    Dimensions,
-    TouchableHighlight,
-    Modal,
-    Text,
-    Button,
-    StyleSheet,
-    Linking
+	View,
+	FlatList,
+	Image,
+	Dimensions,
+	TouchableHighlight,
+	Modal,
+	Text,
+	Button,
+	StyleSheet,
+	Linking
 } from 'react-native';
 import { getImages, removeImage } from '../utils/storage.js';
-import { AnimatedImages } from "../components/AnimatedImages/AnimatedImages";
+import { AnimatedImages } from '../components/AnimatedImages/AnimatedImages';
 import { styles } from '../Styles';
 import { setStringAsync } from 'expo-clipboard';
 
@@ -21,131 +21,140 @@ import AwesomeButton from 'react-native-really-awesome-button/src/themes/blue';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Toast from 'react-native-toast-message';
 
-let { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+let { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export default function GalleryScreen() {
 	const [images, setImages] = useState([]);
 	const [fullImage, setFullImage] = useState({});
 	const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-        let isMounted = true;
-        getImages().then((images) => {
-            if (isMounted) setImages(images);
-        });
-        return () => {
-            isMounted = false;
-        };
-    }, []);
+	useEffect(() => {
+		let isMounted = true;
+		getImages().then((images) => {
+			if (isMounted) setImages(images);
+		});
+		return () => {
+			isMounted = false;
+		};
+	}, []);
 
 	const openImage = (image) => {
 		setFullImage(image);
 		setShowModal(true);
-	}
+	};
 
-    const renderImages = (image) => {
-        return (
+	const renderImages = (image) => {
+		return (
 			<>
-			<Modal
-				transparent={false}
-				visible={showModal}
-				onRequestClose={() => {
-					console.log(fullImage);
-					setFullImage({});
-					setShowModal(false);
-					console.log('Modal has been closed.');
-				}}>
-				{/*All views of Modal*/}
-				{/*Animation can be slide, slide, none*/}
-				<View style={styles.modal}>
-					<Image
-						source={{ uri: fullImage.localUrl }}
-						style={{
-							height: screenWidth,
-							width: screenWidth
-						}}
-					/>
+				<Modal
+					transparent={false}
+					visible={showModal}
+					onRequestClose={() => {
+						console.log(fullImage);
+						setFullImage({});
+						setShowModal(false);
+						console.log('Modal has been closed.');
+					}}>
+					{/*All views of Modal*/}
+					{/*Animation can be slide, slide, none*/}
+					<View style={styles.modal}>
+						<Image
+							source={{ uri: fullImage.localUrl }}
+							style={{
+								height: screenWidth,
+								width: screenWidth
+							}}
+						/>
 
-					<View style={styles.buttonContainerGallery}>
-						<AwesomeButton
-							style={styles.button}
-							size='medium'
-							onPress={() => Linking.openURL(fullImage.url)}
-						>
-							<Text style={{ fontSize: 20, color: 'white' }}>Open in Browser</Text>
-						</AwesomeButton>
-						<AwesomeButton
-							style={styles.button}
-							onPress={async () => {
-								await setStringAsync(fullImage.url).then(() => {
-									Toast.show({
-										type: 'success',
-										text1: 'URL retrieved',
-										text2: `Image URL copied to the clipboard`,
-										position: 'bottom'
-									});
-								}).catch((err) => {
-									console.log(err);
-									Toast.show({
-										type: 'error',
-										text1: 'An error occured!',
-										text2: `${err}`
-									});
-								})
-							}}>
-							<Ionicons
-								style={{ margin: 8, color: 'white' }}
-								name='clipboard-outline'
-								size={30}
-							/>
-						</AwesomeButton>
-						{ fullImage.deleteUrl ?
+						<View style={styles.buttonContainerGallery}>
+							<AwesomeButton
+								style={styles.button}
+								size='medium'
+								onPress={() => Linking.openURL(fullImage.url)}>
+								<Text style={{ fontSize: 20, color: 'white' }}>
+									Open in Browser
+								</Text>
+							</AwesomeButton>
 							<AwesomeButton
 								style={styles.button}
 								onPress={async () => {
-									await Linking.openURL(fullImage.deleteUrl);
-									setImages(await removeImage(fullImage.deleteUrl));
-									setShowModal(false);
-								}}
-							>
+									await setStringAsync(fullImage.url)
+										.then(() => {
+											Toast.show({
+												type: 'success',
+												text1: 'URL retrieved',
+												text2: `Image URL copied to the clipboard`,
+												position: 'bottom'
+											});
+										})
+										.catch((err) => {
+											console.log(err);
+											Toast.show({
+												type: 'error',
+												text1: 'An error occured!',
+												text2: `${err}`
+											});
+										});
+								}}>
 								<Ionicons
-									style={{ margin: 8, color: 'red' }}
-									name='trash-outline'
+									style={{ margin: 8, color: 'white' }}
+									name='clipboard-outline'
 									size={30}
 								/>
-							</AwesomeButton> : null
-						}
+							</AwesomeButton>
+							{fullImage.deleteUrl ? (
+								<AwesomeButton
+									style={styles.button}
+									onPress={async () => {
+										await Linking.openURL(
+											fullImage.deleteUrl
+										);
+										setImages(
+											await removeImage(
+												fullImage.deleteUrl
+											)
+										);
+										setShowModal(false);
+									}}>
+									<Ionicons
+										style={{ margin: 8, color: 'red' }}
+										name='trash-outline'
+										size={30}
+									/>
+								</AwesomeButton>
+							) : null}
+						</View>
+						<Toast />
 					</View>
-					<Toast />
-				</View>
-			</Modal>
-			<AnimatedImages imageIndex={image.index}>
-	        	<View style={{ flex: 1, alignItems: 'flex-start' }}>
-					<TouchableHighlight onPress={() => openImage(image.item)}>
-						<Image
-							source={{ uri: image.item.localUrl }}
-							style={{
-								margin: 2,
-								height: screenWidth / 3.1,
-								width: screenWidth / 3.1
-							}}
-						/>
-					</TouchableHighlight>
-	        	</View>
-			</AnimatedImages>
+				</Modal>
+				<AnimatedImages imageIndex={image.index}>
+					<View style={{ flex: 1, alignItems: 'flex-start' }}>
+						<TouchableHighlight
+							onPress={() => openImage(image.item)}>
+							<Image
+								source={{ uri: image.item.localUrl }}
+								style={{
+									margin: 2,
+									height: screenWidth / 3.1,
+									width: screenWidth / 3.1
+								}}
+							/>
+						</TouchableHighlight>
+					</View>
+				</AnimatedImages>
 			</>
-        )
-    }
+		);
+	};
 
-    return (
-        <SafeAreaView>
-            <FlatList
-                data={images}
-                renderItem={renderImages}
-                keyExtractor={(item, index) => index.toString()}
-                horizontal={false}
-                numColumns={3}
-            />
-        </SafeAreaView>
-    );
+	return (
+		<SafeAreaView>
+			<FlatList
+				data={images}
+				renderItem={renderImages}
+				keyExtractor={(item, index) => index.toString()}
+				horizontal={false}
+				numColumns={3}
+			/>
+		</SafeAreaView>
+	);
 }
